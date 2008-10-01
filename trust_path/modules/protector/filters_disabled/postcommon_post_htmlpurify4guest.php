8f1fbcf5bcf23fef303059f184a8c8511ba8bf69
Vaughan Montgomery <vaughan@impresscms.org>
2008-10-01 20:18:41 +0000

    finish: updated HTMLPuirifier library and compatibility with purifier library with ImpressCMS.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/protector/branches/3.20@5311 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- trust_path/modules/protector/filters_disabled/postcommon_post_htmlpurify4guest.php
+++ trust_path/modules/protector/filters_disabled/postcommon_post_htmlpurify4guest.php
@@ -2,29 +2,49 @@
 
 class protector_postcommon_post_htmlpurify4guest extends ProtectorFilterAbstract {
 
-	var $purifier ;
+	if(defined('ICMS_VERSION_BUILD') <=10 || !file_exists(XOOPS_ROOT_PATH.'/class/icms.htmlpurifier.php'))
+	{
+		var $purifier;
+	}
 
 	function execute()
 	{
-		global $xoopsUser ;
+		global $xoopsUser;
 
 		// HTMLPurifier runs with PHP5 only
-		if( substr( PHP_VERSION , 0 , 1 ) == '4' ) {
-			die( 'Turn postcommon_post_htmlpurify4guest.php off because this filter cannot run with PHP4' ) ;
+		if(substr(PHP_VERSION, 0, 1) == '4') {die('Turn postcommon_post_htmlpurify4guest.php off because this filter cannot run with PHP4');}
+
+		if(is_object($xoopsUser)) {return true;}
+
+		if(!file_exists(XOOPS_ROOT_PATH.'/class/icms.htmlpurifier.php'))
+		{
+			require_once dirname(dirname(__FILE__)).'/library/htmlpurifier/HTMLPurifier.standalone.php' ;
+			require_once dirname(dirname(__FILE__)).'/library/htmlpurifier/HTMLPurifier.autoload.php' ;
+			$config = HTMLPurifier_Config::createDefault();
+			$config->set('Cache', 'SerializerPath', XOOPS_TRUST_PATH.'/modules/protector/configs');
+			$config->set('Core', 'Encoding', _CHARSET);
+			if(strtolower(_CHARSET) !== 'utf-8')
+			{
+				$config->set('Core', 'EscapeNonASCIICharacters', true); // escapes Non ASCII characters that non utf-8 character sets recognise.
+			}
+			//$config->set('HTML', 'Doctype', 'HTML 4.01 Transitional');
+			$this->purifier = new HTMLPurifier($config);
+
+			$_POST = $this->purify_recursive($_POST);
 		}
+		else
+		{
+			if(file_exists(ICMS_ROOT_PATH.'/class/icms.htmlpurifier.php'))
+			{
+				if(!class_exists('icms_HTMLPurifier'))
+				{
+					include_once ICMS_ROOT_PATH.'/class/icms.htmlpurifier.php';
+				}
+				$pure = & icms_HTMLPurifier::getPurifierInstance();
 
-		if( is_object( $xoopsUser ) ) {
-			return true ;
+				$_POST = $pure->icms_html_purifier($_POST, 'protector');
+			}
 		}
-
-		require_once dirname(dirname(__FILE__)).'/library/HTMLPurifier.auto.php' ;
-		$config = HTMLPurifier_Config::createDefault();
-		$config->set('Cache', 'SerializerPath', XOOPS_TRUST_PATH.'/modules/protector/configs');
-		$config->set('Core', 'Encoding', _CHARSET);
-		//$config->set('HTML', 'Doctype', 'HTML 4.01 Transitional');
-		$this->purifier = new HTMLPurifier($config);
-
-		$_POST = $this->purify_recursive( $_POST ) ;
 	}
 
 
