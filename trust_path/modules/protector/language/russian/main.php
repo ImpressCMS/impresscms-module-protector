dbaebc9f1a2df4b7a9487dca1e80ce0d77064529
algalochkin <contact@impresscms.org>
2008-11-29 06:08:20 +0000

    Russian translation of module protector 3.21 constants. A file main.php
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/protector/trunk@7268 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- trust_path/modules/protector/language/russian/main.php
+++ trust_path/modules/protector/language/russian/main.php
@@ -1,7 +1,10 @@
 <?php
 // $Id$ Russian translation. Charset: utf-8 (without BOM)
 define('_MD_PROTECTOR_YOUAREBADIP','Вы зарегистрированы модулем Protector как BAD_IP.') ;
+define('_MD_PROTECTOR_FMT_JAILINFO','"Срок этого ограничения истечет %s') ;
+define('_MD_PROTECTOR_FMT_JAILTIME','Y-m-j H:i:s') ;
+define('_MD_PROTECTOR_BANDWIDTHLIMITED','Скорость доступа к этому сайту слишком  мала сейчас. Повторите позже.') ;
 define('_MD_PROTECTOR_TURNJAVASCRIPTON','Включите поддержку JavaScript') ;
-define('_MD_PROTECTOR_DENYBYRBL','Protector уничтожил Вашу отправку, потому что Ваш IP зарегистрирован в черном списке (RBL)') ;
+define('_MD_PROTECTOR_DENYBYRBL','Protector уничтожил Ваше сообщение, потому что Ваш IP зарегистрирован в черном списке (RBL)') ;
 
 ?>
