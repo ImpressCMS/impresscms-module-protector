4c5bdd72fdb45ac0426b0f1cca24683624ac67b9
Vaughan Montgomery <vaughan@impresscms.org>
2008-12-06 16:57:33 +0000

    adding protector 3.22 branch
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/protector/branches/3.22@7463 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ xoops_trust_path/modules/protector/language/persian/main.php
@@ -0,0 +1,16 @@
+<?php
+/* Translated by stranger @ www.impresscms.ir since version : 3.16
+   Voltan had made another translation for version : 3.02 but this work is totally different (due to too many translating mistakes of him ...)
+*/
+
+
+// Appended by Xoops Language Checker -GIJOE- in 2008-11-21 04:44:31
+define('_MD_PROTECTOR_FMT_JAILINFO','This restriction will be expired on %s');
+define('_MD_PROTECTOR_FMT_JAILTIME','Y-m-j H:i:s');
+define('_MD_PROTECTOR_BANDWIDTHLIMITED','This site is very crowed now. try later.');
+
+define('_MD_PROTECTOR_YOUAREBADIP','شما تحت عنوان یک IP غیرمجاز، توسط محافظ سایت ثبت شده اید.') ;
+define('_MD_PROTECTOR_TURNJAVASCRIPTON','لطفا JavaScript را روشن کنید') ;
+define('_MD_PROTECTOR_DENYBYRBL','محافظ سایت پیام های شما را قبول نمی کند چون IP شما در لیست IP های غیرمجاز قرار دارد') ;
+
+?>
