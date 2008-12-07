33601ad1e9a7022f9f56dfbe197ae15cd92739d3
xoops-magazine <xoops-magazine@tld.com>
2008-12-07 12:53:42 +0000

    german files UTF-8 for new version 3.22
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/protector/branches/3.22@7474 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- xoops_trust_path/modules/protector/language/german/main.php
+++ xoops_trust_path/modules/protector/language/german/main.php
@@ -1,13 +1,8 @@
-<?php
-
-
-// Appended by Xoops Language Checker -GIJOE- in 2008-11-21 04:44:30
-define('_MD_PROTECTOR_FMT_JAILINFO','This restriction will be expired on %s');
-define('_MD_PROTECTOR_FMT_JAILTIME','Y-m-j H:i:s');
-define('_MD_PROTECTOR_BANDWIDTHLIMITED','This site is very crowed now. try later.');
-
-define('_MD_PROTECTOR_YOUAREBADIP','You are registered as BAD_IP by Protector.') ;
-define('_MD_PROTECTOR_TURNJAVASCRIPTON','Turn JavaScript ON') ;
-define('_MD_PROTECTOR_DENYBYRBL','Protector rejects your post, because your IP is registered in RBL') ;
-
+﻿<?php
+define('_MD_PROTECTOR_YOUAREBADIP','Diese IP wurden als Böse-IP vom Protector registriert.') ;
+define('_MD_PROTECTOR_FMT_JAILINFO','Diese Einschränkung verfällt am %s') ;
+define('_MD_PROTECTOR_FMT_JAILTIME','Y-m-j H:i:s') ;
+define('_MD_PROTECTOR_BANDWIDTHLIMITED','Die Webseite ist derzeit überfüllt, bitte versuchen Sie es später erneut.') ;
+define('_MD_PROTECTOR_TURNJAVASCRIPTON','JavaScript einschalten') ;
+define('_MD_PROTECTOR_DENYBYRBL','Protector hat den Beitrag gesperrt da die verwendete IP auf der Ausschlußliste steht') ;
 ?>
