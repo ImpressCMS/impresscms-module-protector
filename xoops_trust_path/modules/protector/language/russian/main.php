6806221e5bbb96af2e081b10489e45599252cbd8
algalochkin <contact@impresscms.org>
2008-12-07 19:13:19 +0000

    Russian translation of constants of module Protector 3.22 (utf-8)
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/protector/branches/3.22@7489 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- xoops_trust_path/modules/protector/language/russian/main.php
+++ xoops_trust_path/modules/protector/language/russian/main.php
@@ -1,13 +1,10 @@
 <?php
-
-
-// Appended by Xoops Language Checker -GIJOE- in 2008-11-21 04:44:31
-define('_MD_PROTECTOR_FMT_JAILINFO','This restriction will be expired on %s');
-define('_MD_PROTECTOR_FMT_JAILTIME','Y-m-j H:i:s');
-define('_MD_PROTECTOR_BANDWIDTHLIMITED','This site is very crowed now. try later.');
-
-define('_MD_PROTECTOR_YOUAREBADIP','You are registered as BAD_IP by Protector.') ;
-define('_MD_PROTECTOR_TURNJAVASCRIPTON','Turn JavaScript ON') ;
-define('_MD_PROTECTOR_DENYBYRBL','Protector rejects your post, because your IP is registered in RBL') ;
+// $Id$ Russian translation. Charset: utf-8 (without BOM)
+define('_MD_PROTECTOR_YOUAREBADIP','Вы зарегистрированы модулем Protector как BAD_IP.') ;
+define('_MD_PROTECTOR_FMT_JAILINFO','"Срок этого ограничения истечет %s') ;
+define('_MD_PROTECTOR_FMT_JAILTIME','Y-m-j H:i:s') ;
+define('_MD_PROTECTOR_BANDWIDTHLIMITED','Скорость доступа к этому сайту слишком  мала сейчас. Повторите позже.') ;
+define('_MD_PROTECTOR_TURNJAVASCRIPTON','Включите поддержку JavaScript') ;
+define('_MD_PROTECTOR_DENYBYRBL','Protector отверг Ваше сообщение, потому что Ваш IP зарегистрирован в черном списке (RBL)') ;
 
 ?>
